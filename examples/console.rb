98a16ca81e7bd929a1b06baf6571ef6c9753c8ae
Piotr Murach <pmurach@gmail.com>
2019-07-07 19:48:45 +0200

    Change display

--- examples/console.rb
+++ examples/console.rb
@@ -6,8 +6,8 @@ logger.with(path: "/var/www/example.com").info("Deploying", "code")
 
 puts "Level loggers:"
 
-logger.debug("Debugging")
-logger.info("For the information about some very long process")
-logger.warn("Are you sure?")
+logger.debug("Debugging deployment")
+logger.info("For the information about the deploy")
+logger.warn("Lack of resources to deploy")
 logger.error("Failed to deploy")
-logger.fatal("Terribley failed to deploy")
+logger.fatal("Terribly failed to deploy")
