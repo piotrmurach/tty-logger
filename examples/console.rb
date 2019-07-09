eb4932dec93ec28e51d69d486b277f25c82ee2d9
Piotr Murach <pmurach@gmail.com>
2019-07-09 20:37:58 +0200

    Change example to be more compact

--- examples/console.rb
+++ examples/console.rb
@@ -7,7 +7,7 @@ logger.with(path: "/var/www/example.com").info("Deploying", "code")
 puts "Level loggers:"
 
 logger.debug("Debugging deployment")
-logger.info("For the information about the deploy")
-logger.warn("Lack of resources to deploy")
+logger.info("Info about the deploy")
+logger.warn("Lack of resources")
 logger.error("Failed to deploy")
 logger.fatal("Terribly failed to deploy")
