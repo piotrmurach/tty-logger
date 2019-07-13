3a3d2210fc515b6021bfd2314bf5a1c2f69a9e4a
Piotr Murach <pmurach@gmail.com>
2019-07-13 17:33:32 +0200

    Add console styles override example

---
+++ examples/override.rb
@@ -0,0 +1,29 @@
+require_relative "../lib/tty/logger"
+
+logger = TTY::Logger.new
+
+logger.success("Default success")
+logger.error("Default error")
+
+puts
+
+new_style = TTY::Logger.new do |config|
+  config.handlers = [
+    [:console, {
+      styles: {
+        success: {
+          symbol: "+",
+          label: "Ohh yes"
+        },
+        error: {
+          symbol: "!",
+          label: "Dooh",
+          levelpad: 3
+        }
+      }
+    }]
+  ]
+end
+
+new_style.success("Custom success")
+new_style.error("Custom error")
