0e28130ebc4ef3d1217cd355db248dfb9908201f
Piotr Murach <pmurach@gmail.com>
2019-07-18 20:22:45 +0200

    Add max_bytes and max_depth to json formatter

--- lib/tty/logger/formatters/json.rb
+++ lib/tty/logger/formatters/json.rb
@@ -5,9 +5,58 @@ require "json"
 module TTY
   class Logger
     module Formatters
+      # Format data suitable for data exchange
       class JSON
-        def dump(obj, options = {})
-          ::JSON.generate(obj)
+        ELLIPSIS = "..."
+
+        # Dump data into a JSON formatted string
+        #
+        # @param [Hash] obj
+        #   the object to serialize as JSON
+        #
+        # @return [String]
+        #
+        # @api public
+        def dump(obj, max_bytes: 2**12, max_depth: 3)
+          bytesize = 0
+
+          hash = obj.reduce({}) do |acc, (k, v)|
+            str = (k.to_json + v.to_json)
+            items = acc.keys.size - 1
+
+            if bytesize + str.bytesize + items + ELLIPSIS.bytesize > max_bytes
+              acc[k] = ELLIPSIS
+              break acc
+            else
+              bytesize += str.bytesize
+              acc[k] = dump_val(v, max_depth)
+            end
+            acc
+          end
+          ::JSON.generate(hash)
+        end
+
+        private
+
+        def dump_val(val, depth)
+          case val
+          when Hash then enc_obj(val, depth - 1)
+          when Array then enc_arr(val, depth - 1)
+          else
+            val
+          end
+        end
+
+        def enc_obj(obj, depth)
+          return ELLIPSIS if depth.zero?
+
+          obj.reduce({}) { |acc, (k, v)| acc[k] = dump_val(v, depth); acc }
+        end
+
+        def enc_arr(obj, depth)
+          return ELLIPSIS if depth.zero?
+
+          obj.reduce([]) { |acc, v| acc << dump_val(v, depth); acc }
         end
       end # JSON
     end # Formatters
--- spec/unit/formatters/json_spec.rb
+++ spec/unit/formatters/json_spec.rb
@@ -12,4 +12,30 @@ RSpec.describe TTY::Logger::Formatters::JSON, "#dump" do
 
     expect(formatter.dump(data)).to eq("{\"app\":\"myapp\",\"env\":\"prod\",\"sql\":\"SELECT * FROM admins\",\"at\":\"1970-01-02 10:17:36 UTC\"}")
   end
+
+  [
+    {obj: {a: "aaaaa", b: "bbbbb", c: "ccccc"}, bytes: 3*12+2, want: "{\"a\":\"aaaaa\",\"b\":\"bbbbb\",\"c\":\"ccccc\"}"},
+    {obj: {a: "aaaaa", b: "bbbbb", c: "ccccc"}, bytes: 2*12+4, want: "{\"a\":\"aaaaa\",\"b\":\"bbbbb\",\"c\":\"...\"}"},
+    {obj: {a: "aaaaa", b: "bbbbb", c: "ccccc"}, bytes: 12+4, want: "{\"a\":\"aaaaa\",\"b\":\"...\"}"},
+    {obj: {a: "aaaaa", b: "bbbbb", c: "ccccc"}, bytes: 11, want: "{\"a\":\"...\"}"},
+  ].each do |data|
+    it "truncates #{data[:obj].inspect} to #{data[:want].inspect} of #{data[:bytes]} bytes" do
+      formatter = described_class.new
+      expect(formatter.dump(data[:obj], max_bytes: data[:bytes])).to eq(data[:want])
+    end
+  end
+
+  [
+    {obj: {a: {b: {c: "ccccc"}}}, depth: 1, want: "{\"a\":\"...\"}"},
+    {obj: {a: {b: {c: "ccccc"}}}, depth: 2, want: "{\"a\":{\"b\":\"...\"}}"},
+    {obj: {a: {b: {c: "ccccc"}}}, depth: 3, want: "{\"a\":{\"b\":{\"c\":\"ccccc\"}}}"},
+    {obj: {a: ["b", {c: "ccccc"}]}, depth: 1, want: "{\"a\":\"...\"}"},
+    {obj: {a: ["b", {c: "ccccc"}]}, depth: 2, want: "{\"a\":[\"b\",\"...\"]}"},
+    {obj: {a: ["b", {c: "ccccc"}]}, depth: 3, want: "{\"a\":[\"b\",{\"c\":\"ccccc\"}]}"},
+  ].each do |data|
+    it "truncates nested object #{data[:obj].inspect} to #{data[:want].inspect}" do
+      formatter = described_class.new
+      expect(formatter.dump(data[:obj], max_depth: data[:depth])).to eq(data[:want])
+    end
+  end
 end
