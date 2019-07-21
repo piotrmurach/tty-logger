288d2812092b5299286ab44e70334c16a220879e
Piotr Murach <pmurach@gmail.com>
2019-07-21 19:54:07 +0200

    Add file output example

---
+++ examples/output.rb
@@ -0,0 +1,15 @@
+require_relative "../lib/tty/logger"
+
+file = File.open("errors.log", "a")
+
+TTY::Logger.configure do |config|
+  config.metadata = [:all]
+  config.handlers = [:stream]
+  config.output = file
+end
+
+logger = TTY::Logger.new(fields: {app: "myapp", env: "prod"})
+
+logger.error("Failed to deploy")
+
+file.close
