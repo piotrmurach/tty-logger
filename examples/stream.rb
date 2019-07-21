8ad9d380fdc91dc5e73f037a529184aaa5d3edc5
Piotr Murach <pmurach@gmail.com>
2019-07-21 14:51:33 +0200

    Add stream handler example

---
+++ examples/stream.rb
@@ -0,0 +1,22 @@
+require_relative "../lib/tty/logger"
+
+TTY::Logger.configure do |config|
+  config.max_bytes = 2**5
+  config.metadata = [:all]
+  config.handlers = [[:stream, formatter: :text]]
+  config.level = :debug
+end
+
+logger = TTY::Logger.new(fields: {app: "myapp", env: "prod", extra: 'yes'})
+
+logger.with(path: "/var/www/example.com").info("Deploying", "code")
+
+puts "Levels:"
+
+logger.debug("Debugging deployment")
+logger.info("Info about the deploy")
+logger.warn("Lack of resources")
+logger.error("Failed to deploy")
+logger.fatal("Terribly failed to deploy")
+logger.success("Deployed successfully")
+logger.wait("Ready to deploy")
