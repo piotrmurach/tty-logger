a827c82c4b09dbcd9538bfed0fcd1520063c20c1
Piotr Murach <pmurach@gmail.com>
2019-07-21 17:36:13 +0200

    Add test for specifying a handler specific output

--- spec/unit/output_spec.rb
+++ spec/unit/output_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Logger, "outputs" do
+  let(:styles) { TTY::Logger::Handlers::Console::STYLES }
+
   it "outputs to multiple streams of the same handler" do
     stream = StringIO.new
 
@@ -13,6 +15,25 @@ RSpec.describe TTY::Logger, "outputs" do
 
     expect(stream.string).to eq([
       "level=info message=logging\n",
+      "level=info message=logging\n"
+    ].join)
+  end
+
+  it "outputs each handler to a different stream" do
+    stream = StringIO.new
+
+    logger = TTY::Logger.new do |config|
+      config.handlers = [
+        [:console, output: stream],
+        [:stream, output: stream]
+      ]
+    end
+
+    logger.info("logging")
+
+    expect(stream.string).to eq([
+      "\e[32m#{styles[:info][:symbol]}\e[0m \e[32minfo\e[0m    ",
+      "logging                  \n",
       "level=info message=logging\n"
     ].join)
   end
