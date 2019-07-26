7af2242f6fd8bafaea59a60247989f046e0c0ef3
Piotr Murach <pmurach@gmail.com>
2019-07-26 21:09:29 +0200

    Add perf tests for formatting hashes with 2048 keys

--- spec/perf/json_formatter_spec.rb
+++ spec/perf/json_formatter_spec.rb
@@ -5,7 +5,18 @@ require "rspec-benchmark"
 RSpec.describe TTY::Logger::Formatters::JSON do
   include RSpec::Benchmark::Matchers
 
-  it "compares JSON formatter with the native JSON serializer" do
+  it "formats large hashes(2048 keys) 2x faster with the native JSON" do
+    large_data = Hash[Array.new(2048) { |i| [i + 1, "hey"] }]
+    formatter = described_class.new
+
+    expect {
+      ::JSON.dump(large_data)
+    }.to perform_faster_than {
+      formatter.dump(large_data)
+    }.at_least(2).times
+  end
+
+  it "formats large values 2x faster with the native JSON" do
     large_data = { "foo" => "b#{'a'*2048}" }
     formatter = described_class.new
 
--- spec/perf/text_formatter_spec.rb
+++ spec/perf/text_formatter_spec.rb
@@ -5,7 +5,18 @@ require "rspec-benchmark"
 RSpec.describe TTY::Logger::Formatters::Text do
   include RSpec::Benchmark::Matchers
 
-  it "compares text formatter with the native JSON serializer" do
+  it "formats large hashes(2048 keys) 1.25x faster with the native JSON" do
+    large_data = Hash[Array.new(2048) { |i| [i + 1, "hey"] }]
+    formatter = described_class.new
+
+    expect {
+      ::JSON.dump(large_data)
+    }.to perform_faster_than {
+      formatter.dump(large_data)
+    }.at_least(1.25).times
+  end
+
+  it "formats large values 4x faster with the native JSON" do
     large_data = { "foo" => "b#{'a'*2048}" }
     formatter = described_class.new
 
