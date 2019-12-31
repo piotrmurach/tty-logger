23380409e52cd767e7103c97723783dbbfc465cd
Piotr Murach <pmurach@gmail.com>
2019-12-31 19:53:43 +0100

    Change to fix filter example to use new api

--- examples/filter.rb
+++ examples/filter.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/tty/logger"
 
 logger = TTY::Logger.new do |config|
-  config.data_filters = [/^foo\.ba/]
+  config.filters.data = [/^foo\.ba/]
 end
 
 logger.info("Filtering data", {"foo" => {"bar" => "val"}, "baz" => ["bar", "val"]})
